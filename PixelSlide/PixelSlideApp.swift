9dd28b2d2fa6e05a5a47f8480329cf4a1dfe0bed
Nicolas Chambon <nicolaschambon@Mac.lan>
2025-03-18 16:01:54 +0000

    Initial commit

---
+++ PixelSlide/PixelSlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelSlideApp.swift
+//  PixelSlide
+//
+//  Created by Nicolas Chambon on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PixelSlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PixelSlideTests/PixelSlideTests.swift
@@ -0,0 +1,17 @@
+//
+//  PixelSlideTests.swift
+//  PixelSlideTests
+//
+//  Created by Nicolas Chambon on 18/03/2025.
+//
+
+import Testing
+@testable import PixelSlide
+
+struct PixelSlideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
